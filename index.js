574a4914043434eff43828ede14ebcd8d7fdfd60
Caitlin Dwyer <caitlin.dwyer@salesforce.com>
2019-02-13 18:34:15 -0500

    everything

--- index.js
+++ index.js
@@ -2,12 +2,97 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+const request = require("request");
+/*
+{{authEndpoint}}/v1/requestToken
+*/
+const auth_options = {
+    method: 'POST',
+    url: 'https://auth.exacttargetapis.com/v1/requestToken',
+    headers: {
+        'Postman-Token': 'fa90cab9-64bc-483d-ba38-8279dcc10142',
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json'
+    },
+    body: {
+        clientId: 'ly953984jrib62wrltaa4d99',
+        clientSecret: 'B1ssz6lsOVtwY6dSP3IXFn9j'
+    },
+    json: true
+};
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool())) 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({
+        extended: true
+    }))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/cool', (req, res) => res.send(cool()))
+    .get('/helloworld', (req, res) => res.send('hello world'))
+    .get('/form', (req, res) => res.render('pages/form'))
+    .post('/form', function(req, res) {
+        console.log(req.body);
+        let phoneDigits = req.body.phone;
+        // logic to ping MC & SC here
+        request(auth_options, function(error, response, body) {
+            if (error) throw new Error(error);
+            console.log(body);
+            let token = "Bearer " + body.accessToken;
 
+            const sms_options = {
+                method: 'POST',
+                url: 'https://www.exacttargetapis.com/sms/v1/messageContact/MTo3ODow/send',
+                headers: {
+                    'Postman-Token': '5e8fb9d9-4b73-42bb-895f-8e5583a93d4a',
+                    'Cache-Control': 'no-cache',
+                    'Content-Type': 'application/json',
+                    Authorization: token
+                },
+                body: {
+                    Subscribe: true,
+                    Resubscribe: true,
+                    mobileNumbers: [phoneDigits],
+                    keyword: 'SNAKES',
+                    Override: true,
+                    messageText: "Throw me the idol! I'll throw you the whip!"
+                },
+                json: true
+            };
+            // next call
+            request(sms_options, function (err, r, b) {
+			    if (err) throw new Error(err);
+
+			    console.log(b);
+			});
+        });
+        res.send('Thanks for submitting a form');
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+/*
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'https://auth.exacttargetapis.com/v1/requestToken',
+  headers: 
+   { 'Postman-Token': 'b513afc7-fa50-47c6-8f7f-20ea89fff653',
+     'Cache-Control': 'no-cache',
+     'Content-Type': 'application/json' },
+  body: 
+   { clientId: 'q4upcyyfbed1ldmj4j8u4l54',
+     clientSecret: 'CHh4P3z2icCxKKjUq54q8Cd4' },
+  json: true };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+*/
